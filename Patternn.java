d21ce6272c43c61621b78edfef289aedd8c9c264
Manan-0107 <mananshah1127@gmail.com>
2026-07-30 12:11:09 +0530

    Update Patternn.java

--- Patternn.java
+++ Patternn.java
@@ -4,7 +4,7 @@ public static void main (String[] args){
 Scanner s = new Scanner(System.in);
 int n = s.nextInt();
 for(int i=0;i<n;i++){
-for(int j=n-i;j>0;j--){
+for(int j=n-i-1;j>0;j--){
 System.out.print(' ');
 }
 for(int r = 0;r<i+1;r++){
