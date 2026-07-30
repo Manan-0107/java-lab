5c9ec2ca3170d36a9d6488b6fdff1b7141c498ef
Manan-0107 <mananshah1127@gmail.com>
2026-07-30 11:52:21 +0530

    Add files via upload

---
+++ Patternn.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Patternn{
+public static void main (String[] args){
+Scanner s = new Scanner(System.in);
+int n = s.nextInt();
+for(int i=0;i<n;i++){
+for(int j=n-i;j>0;j--){
+System.out.print(' ');
+}
+for(int r = 0;r<i+1;r++){
+System.out.print('*');
+System.out.print(' ');
+}
+System.out.println('\n');
+}
+for(int k=n-1;k>0;k--){
+for(int l=k-1;l<n;l++){
+System.out.print(' ');
+}
+for(int h =0;h<k;h++){
+System.out.print('*');
+System.out.print(' ');
+}
+System.out.println('\n');
+}
+}
+}
